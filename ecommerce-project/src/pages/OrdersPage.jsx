93d004ddde112527eae2d6ffd6a044b90e713478
sardarabhay <sardarabhay90@gmail.com>
2025-12-17 16:23:55 +0000

    Change the icon in the tabs using react

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -5,6 +5,7 @@ export function OrdersPage(){
 
     return(
         <>
+            <link rel="icon" type="image/svg+xml" href="orders-favicon.png" />
             <title>Orders</title>
             <Header />
 
