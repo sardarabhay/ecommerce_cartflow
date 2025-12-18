c7f376f62d30a94c9b26ee8fe8b616ed8f02d466
sardarabhay <sardarabhay90@gmail.com>
2025-12-18 09:19:06 +0000

    Lift cart up and share it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,19 +1,30 @@
-import {Routes,Route} from 'react-router';
-import {HomePage} from './pages/HomePage.jsx';
+import { Routes, Route } from 'react-router';
+import  axios from 'axios';
+import { useEffect, useState } from 'react';
+import { HomePage } from './pages/HomePage.jsx';
 import { CheckoutPage } from './pages/checkout/CheckoutPage.jsx';
 import { OrdersPage } from './pages/OrdersPage.jsx';
 import { TrackingPage } from './pages/TrackingPage.jsx';
+
 
 import './App.css'
 import { NotFoundPage } from './pages/NotFoundPage.jsx';
 
 function App() {
-  
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/cart-items')
+      .then((response) => {
+        setCart(response.data);
+      });
+
+  }, []);
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
-      <Route path ="orders" element={<OrdersPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,18 +5,13 @@ import { Header } from '../components/Header';
 import CheckMark from '../assets/images/icons/checkmark.png';
 import './HomePage.css'
 
-export function HomePage() {
+export function HomePage({cart}) {
     const[products,setProducts]=useState([]);
-    const [cart,setCart]=useState([]);
+    
     useEffect(() => {
         axios.get('/api/products')
             .then((response) => {
                 setProducts(response.data);
-            });
-
-        axios.get('/api/cart-items')
-            .then((response)=>{
-                setCart(response.data);
             });
     },[]);
 
