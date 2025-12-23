36daabd10262ea09a3be8bf9741df5ba12a6c4c9
sardarabhay <sardarabhay90@gmail.com>
2025-12-23 15:45:59 +0000

    Create new react Typescipt project

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -3,18 +3,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:3000'
+  plugins: [react({
+    babel: {
+      plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+    },
+  })],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
       },
-      '/images':{
-        target:'http://localhost:3000'
+      '/images': {
+        target: 'http://localhost:3000'
       },
     }
   },
-  build:{
-    outDir:'../ecommerce-backend/dist'
+  build: {
+    outDir: '../ecommerce-backend/dist'
   }
 })
